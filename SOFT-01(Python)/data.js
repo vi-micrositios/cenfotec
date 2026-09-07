1cde109e896e17baec47b37e8e10c8eab62b60df
Verónica Mora Lezcano <vmora@ucenfotec.ac.cr>
2026-09-07 14:01:20 -0600

    [SOFT-01] Módulo 01

--- SOFT-01(Python)/data.js
+++ SOFT-01(Python)/data.js
@@ -224,39 +224,39 @@ const data = {
 
                                 <div style=" display:flex; flex-wrap:wrap; align-items:center; justify-content:center; gap:8px;
                                 ">
-                            <!-- PROBLEMA --> <div style="     padding:12px 15px;     border:2px solid #c81f66;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-lightbulb"        style="font-size:25px;color:#c81f66;"></i>
+                            <!-- PROBLEMA --> <div style="     padding:12px 15px;     border:2px solid #712c86;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-lightbulb"        style="font-size:25px;color:#c81f66;"></i>
                                 <div style="margin-top:6px;">         <b>Problema</b><br>         <small>Necesidad a resolver</small>     </div> </div>
                             <span style="color:#00928d;font-size:20px;">→</span>
 
                             <!-- ALGORITMO --> <div style="     padding:12px 15px;     border:2px solid #00928d;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-brain"        style="font-size:25px;color:#00928d;"></i>
                                 <div style="margin-top:6px;">         <b>Algoritmo</b><br>         <small>Pasos para resolverlo</small>     </div> </div>
-                            <span style="color:#c81f66;font-size:20px;">→</span>
+                            <span style="color:#712c86;font-size:20px;">→</span>
 
-                            <!-- CÓDIGO --> <div style="     padding:12px 15px;     border:2px solid #c81f66;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-code"        style="font-size:25px;color:#c81f66;"></i>
+                            <!-- CÓDIGO --> <div style="     padding:12px 15px;     border:2px solid #712c86;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-code"        style="font-size:25px;color:#c81f66;"></i>
                                 <div style="margin-top:6px;">         <b>Código</b><br>         <small>Solución escrita</small>     </div> </div>
                             <span style="color:#00928d;font-size:20px;">→</span>
 
                             <!-- EDITAR --> <div style="     padding:12px 15px;     border:2px solid #00928d;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-pen-to-square"        style="font-size:25px;color:#00928d;"></i>
                                 <div style="margin-top:6px;">         <b>Editar</b><br>         <small>Modificar el código</small>     </div> </div>
-                            <span style="color:#c81f66;font-size:20px;">→</span>
+                            <span style="color:#712c86;font-size:20px;">→</span>
 
-                            <!-- COMPILAR --> <div style="     padding:12px 15px;     border:2px solid #c81f66;     border-radius:12px;     min-width:120px; ">     <i class="fa-solid fa-gears"        style="font-size:25px;color:#c81f66;"></i>
+                            <!-- COMPILAR --> <div style="     padding:12px 15px;     border:2px solid #712c86;     border-radius:12px;     min-width:120px; ">     <i class="fa-solid fa-gears"        style="font-size:25px;color:#c81f66;"></i>
                                 <div style="margin-top:6px;">         <b>Compilar</b><br>         <small>Transformar el código</small>     </div> </div>
                             <span style="color:#00928d;font-size:20px;">→</span>
 
                             <!-- INTERPRETAR --> <div style="     padding:12px 15px;     border:2px solid #00928d;     border-radius:12px;     min-width:120px; ">     <i class="fa-solid fa-language"        style="font-size:25px;color:#00928d;"></i>
                                 <div style="margin-top:6px;">         <b>Interpretar</b><br>         <small>Procesar instrucciones</small>     </div> </div>
-                            <span style="color:#c81f66;font-size:20px;">→</span>
+                            <span style="color:#712c86;font-size:20px;">→</span>
 
-                            <!-- ENLAZAR --> <div style="     padding:12px 15px;     border:2px solid #c81f66;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-link"        style="font-size:25px;color:#c81f66;"></i>
+                            <!-- ENLAZAR --> <div style="     padding:12px 15px;     border:2px solid #712c86;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-link"        style="font-size:25px;color:#c81f66;"></i>
                                 <div style="margin-top:6px;">         <b>Enlazar</b><br>         <small>Unir las diferentes piezas</small>     </div> </div>
                             <span style="color:#00928d;font-size:20px;">→</span>
 
                             <!-- EJECUTAR --> <div style="     padding:12px 15px;     border:2px solid #00928d;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-play"        style="font-size:25px;color:#00928d;"></i>
                                 <div style="margin-top:6px;">         <b>Ejecutar</b><br>         <small>Poner el programa en marcha</small>     </div> </div>
-                            <span style="color:#c81f66;font-size:20px;">→</span>
+                            <span style="color:#712c86;font-size:20px;">→</span>
 
-                            <!-- RESULTADO --> <div style="     padding:12px 15px;     border:2px solid #c81f66;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-circle-check"        style="font-size:25px;color:#c81f66;"></i>
+                            <!-- RESULTADO --> <div style="     padding:12px 15px;     border:2px solid #712c86;     border-radius:12px;     min-width:105px; ">     <i class="fa-solid fa-circle-check"        style="font-size:25px;color:#c81f66;"></i>
                                 <div style="margin-top:6px;">         <b>Resultado</b><br>         <small>Solución obtenida</small>     </div> </div>
 
                                 </div>
